bad87cce8aeac7a52377bfff795eb30290e636ca
gabripo <gabripo93+github@gmail.com>
2022-07-19 10:15:02 +0200

    Create abstract class List

---
+++ List/List.h
@@ -0,0 +1,23 @@
+template<typename T>
+class List
+{
+    private:
+        void operator=(const List&);
+        List(const List&);
+    public:
+        List();
+        ~List();
+
+        virtual void clear();
+        virtual void insert(const T& itemToInsert) = 0;
+        virtual void append(const T& itemToAppend) = 0;
+        virtual T remove() = 0;
+        virtual void moveToStart() = 0;
+        virtual void moveToEnd() = 0;
+        virtual void prev() = 0;
+        virtual void next() = 0;
+        virtual int length() const = 0;
+        virtual int currentPos() const = 0;
+        virtual void moveToPos(int pos& const) = 0;
+        virtual const T& getValue() const = 0;
+};
