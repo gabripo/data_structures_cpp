36cd546d745c1a595a1e866bf7e5acee28b8c7a6
gabripo <gabripo93+github@gmail.com>
2022-07-19 11:58:49 +0200

    Functions to test methods of AList

--- AList/test.cpp
+++ AList/test.cpp
@@ -4,5 +4,32 @@ int main(int argc, char const *argv[])
 {
     AList<int> list;
 
+    std::cout << "List length: " << list.length() << std::endl;
+    std::cout << "Current value: " << list.getValue() << std::endl;
+
+    list.clear();
+    list.append(10);
+    list.append(11);
+    list.print();
+    std::cout << "List length: " << list.length() << std::endl;
+    std::cout << "Current value: " << list.getValue() << std::endl;
+
+    list.moveToEnd();
+    list.insert(1);
+    list.append(2);
+    list.print();
+    std::cout << "List length: " << list.length() << std::endl;
+    std::cout << "Current value: " << list.getValue() << std::endl;
+    
+    list.moveToStart();
+    list.print();
+    std::cout << "List length: " << list.length() << std::endl;
+    std::cout << "Current value: " << list.getValue() << std::endl;
+    
+    list.moveToEnd();
+    list.print();
+    std::cout << "List length: " << list.length() << std::endl;
+    std::cout << "Current value: " << list.getValue() << std::endl;
+
     return 0;
 }
