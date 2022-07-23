faa53fde67bc954ff3068212642c6e9a0232cab3
gabripo <gabripo93+github@gmail.com>
2022-07-23 17:28:25 +0200

    Test code for LList.insert() , LList.moveToEnd(), LList.moveToStart(), LList.moveToPos()

--- LList/test.cpp
+++ LList/test.cpp
@@ -17,5 +17,34 @@ int main(int argc, char const *argv[])
     std::cout << "Current position: " << list.currentPos() << std::endl;
     std::cout << "-----------" << std::endl;
 
+    list.insert(0);
+    list.insert(1);
+    list.print();
+    std::cout << "List length: " << list.length() << std::endl;
+    std::cout << "Current value: " << list.getValue() << std::endl;
+    std::cout << "Current position: " << list.currentPos() << std::endl;
+    std::cout << "-----------" << std::endl;
+
+    list.moveToEnd();
+    list.print();
+    std::cout << "List length: " << list.length() << std::endl;
+    std::cout << "Current value: " << list.getValue() << std::endl;
+    std::cout << "Current position: " << list.currentPos() << std::endl;
+    std::cout << "-----------" << std::endl;
+
+    list.moveToStart();
+    list.print();
+    std::cout << "List length: " << list.length() << std::endl;
+    std::cout << "Current value: " << list.getValue() << std::endl;
+    std::cout << "Current position: " << list.currentPos() << std::endl;
+    std::cout << "-----------" << std::endl;
+
+    list.moveToPos(2);
+    list.print();
+    std::cout << "List length: " << list.length() << std::endl;
+    std::cout << "Current value: " << list.getValue() << std::endl;
+    std::cout << "Current position: " << list.currentPos() << std::endl;
+    std::cout << "-----------" << std::endl;
+
     return 0;
 }
