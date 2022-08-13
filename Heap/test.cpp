84da70ca716e4c4eac63c24aa73d88f5d539c4bc
gabripo <gabripo93+github@gmail.com>
2022-08-13 20:59:42 +0200

    Maximum heap size as const int

--- Heap/test.cpp
+++ Heap/test.cpp
@@ -2,12 +2,14 @@
 #include "MaxOperator.cpp"
 #include <iostream>
 
+const int maximumSize = 10;
+
 int main(int argc, char const *argv[])
 {
     int array[] = { 4, 10, 1, 2, 0 };
     int arraySize = sizeof(array) / sizeof(array[0]);
 
-    Heap<int, MaxOperator<int> > maxHeap(array, arraySize, 10);
+    Heap<int, MaxOperator<int> > maxHeap(array, arraySize, maximumSize);
     maxHeap.print();
 
     return 0;
