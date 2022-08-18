a28186b81b5bc051e62e6bac576843302aa97d4b
gabripo <gabripo93+github@gmail.com>
2022-08-18 10:21:58 +0200

    Graph abstract class

---
+++ Graph/Graph.h
@@ -0,0 +1,20 @@
+class Graph {
+    private:
+        void operator=(const Graph&);
+        Graph(const Graph&);
+    public:
+        Graph();
+        virtual ~Graph();
+
+        virtual void init(int numVertices) = 0;
+        virtual int getVertices() = 0;
+        virtual int getEdges() = 0;
+        virtual int getFirstNeighbour(int idxVertex) = 0;
+        virtual int getNextNeighbour(int idxVertex1, int idxVertex2) = 0;
+        virtual void setEdgeWeight(int idxVertex1, int idxVertex2, int edgeWeightToSet) = 0;
+        virtual int getEdgeWeight(int idxVertex1, int idxVertex2) = 0;
+        virtual void delEdge(int idxVertex1, int idxVertex2) = 0;
+        virtual bool isEdge(int idxVertex1, int idxVertex2) = 0;
+        virtual int getMark(int idxVertex) = 0;
+        virtual void setMark(int idxVertex, int markValueToSet) = 0;
+};
